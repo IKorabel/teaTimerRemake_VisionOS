fee0e284f80b1f933e420d9ecbd7684645ea2e6e
Igor Korabelnikov <i.korabel2001@gmail.com>
2024-04-04 19:44:42 +0200

    Refactoring. Entities update

---
+++ TeaTimerRemakeV2/ImmersiveTeaBrewingScene/Entities /ImmersiveTeaBrewingBackground.swift
@@ -0,0 +1,8 @@
+//
+//  ImmersiveTeaBrewingBackground.swift
+//  TeaTimerRemakeV2
+//
+//  Created by Igor Shefer on 04.04.24.
+//
+
+import Foundation
---
+++ TeaTimerRemakeV2/ImmersiveTeaBrewingScene/ImmersiveTeaBrewingView.swift
@@ -0,0 +1,18 @@
+//
+//  ImmersiveTeaBrewingView.swift
+//  TeaTimerRemakeV2
+//
+//  Created by Igor Shefer on 02.04.24.
+//
+
+import SwiftUI
+
+struct ImmersiveTeaBrewingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImmersiveTeaBrewingView()
+}
